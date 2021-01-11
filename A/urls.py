b209fb78841561ef7cba1edfac79fdcd5ea4002a
Bahamin <abbasi.bahamin@gmail.com>
2021-01-11 07:54:41 +0330

    url patterns dep

--- A/urls.py
+++ A/urls.py
@@ -28,3 +28,12 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+if not settings.DEBUG:
+    urlpatterns += url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT})
+    urlpatterns += url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT})
+
+
+# url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+# url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
