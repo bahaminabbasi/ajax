2069c332efd5dc3dd0648d20bab30160b7fdb609
chengu <chenguquab@gmail.com>
2021-01-12 09:51:01 +0330

    accounts app for nothing

--- main/views.py
+++ main/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User   
 
 
 def main(request):
+    print(request)
+    users = User.objects.all()
+    print('users: ', users)
+    user = request.user
+    print('user  : ', user)
     return render(request, 'main/main.html')
